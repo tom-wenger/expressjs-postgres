77a568a728ae89a51906ea3ab6fe0e9ae21d9a86
Tom Wenger <94558839+tom-wenger@users.noreply.github.com>
2023-04-07 15:40:20 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+  const { rows } = await pool.query("SELECT * FROM user");
+  res.send(`${JSON.stringify(rows)}`);
 });
 
 app.listen(port, () => {
